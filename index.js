17ddbcf86537276c03b820e1ce595fb94e0bbeb3
anish <anishvisaria98@gmail.com>
2016-11-12 23:51:17 -0500

    gfdgdf

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-var express = require('express')
+var express = require('express');
 var Bing = require('node-bing-api')({ accKey: "a43582571a4946d0a3e7afb34d09807d" });
-var app = express()
+var urlToImage = require('url-to-image');
+var fs = require('fs');
+var app = express();
 
 app.get('/', function (req, res, next) {
-  //res.send('Hello World!')
-  Bing.web("Pizza", {
-    top: 10,  // Number of results (max 50) 
-    skip: 0   // Skip first 3 results 
-  }, function(error, temp, body){
-    console.log(body.webPages.value);
-  });
-})
+	
+  	Bing.web("Tiger", {
+		top: 30,  // Number of results (max 50) 
+		skip: 0   // Skip first 3 results 
+  	}, function(error, temp, body) {
+		var arr = body.webPages.value.map(function(obj) {
+			urlToImage(obj['displayUrl'], 'google.png').then(function() {
+				var base64str = base64_encode('google.png');
+				return {url: obj['displayUrl'], title: obj['name'], base64: base64str};
+			}).catch(function(err) {
+				console.error(err);
+			});			
+		});
+		res.send(arr); 
+  	});
+});
+
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })	
