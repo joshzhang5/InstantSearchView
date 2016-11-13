dbe6f34f510d55ec587997b67832078186b72c5e
jzhang761 <joshzhang@gatech.edu>
2016-11-13 08:10:32 -0500

    suggestions search

--- hello.js
+++ hello.js
@@ -1,19 +1,8 @@
-var webshot = require('webshot');
-var options = {
-  onLoadFinished: {
-    fn: function(status) {
-      var tags = document.getElementsByTagName(this.tagToReplace);
-
-      for (var i=0; i<tags.length; i++) {
-        var tag = tags[i];
-        tag.innerHTML = 'The loading status of this page is: ' + status;
-      }
-    }
-  , context: {tagToReplace: 'h1'}
-  }
+xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", "https://api.cognitive.microsoft.com/bing/v5.0/Suggestions");
+xmlhttp.setRequestHeader("Ocp-Apim-Subscription-Key", ab77602f5bd44f8392832be857240950);
+xmlhttp.onload = function () {
+  alert(xmlhttp.response);
 };
+xmlhttp.send();
 
-webshot('www.google.com', 'example.png', options, function(err) {
-  if (err) return console.log(err);
-  console.log('OK');
-});
